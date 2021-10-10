893dc72380af2d690f04df1aafde91527f70bed1
faheelsattar <faheelsattar@yahoo.com>
2021-10-10 15:28:15 +0500

    updated: <Roadmap>

--- src/components/RoadMap/RoadMap.tsx
+++ src/components/RoadMap/RoadMap.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 import cryptorBg from "../../assets/cryptor-bg.svg";
+import Carousel from "../common/Carousel";
 const RoadMap: React.FC = () => {
   return (
     <div
-      className="flex flex-col w-full text-white text-center pt-20 sm:mt-28 space-y-5"
+      className="w-full text-white text-center pt-20 sm:mt-28 space-y-5"
       id="roadmap"
     >
       <h1 className="text-2xl">Roadmap</h1>
       <p className="text-sm font-light"></p>
-      <div>
-        <img
+      <div
+        style={{
+          backgroundImage: `url(${cryptorBg})`,
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        {/* <img
           src={cryptorBg}
           className=""
           alt=""
           style={{ width: "70%", display: "block", margin: "0 auto" }}
-          
-        />
+        /> */}
+        <Carousel />
       </div>
     </div>
   );
--- src/components/WhatWeDo/WhatWeDo.tsx
+++ src/components/WhatWeDo/WhatWeDo.tsx
@@ -7,38 +7,49 @@ import payment from "../../assets/feature/payment-sys.svg";
 import track from "../../assets/feature/track-gem.svg";
 
 const WhatWeDo: React.FC = () => {
-  type Features = Array<{ icon: string; heading: String; subHeading: String }>;
+  type Features = Array<{
+    id: number;
+    icon: string;
+    heading: String;
+    subHeading: String;
+  }>;
   const features: Features = [
     {
+      id: 1,
       icon: diligence,
       heading: "DUE DILIGENCE",
       subHeading: "Get all necessary informationsat on place and stay safe",
     },
     {
+      id: 2,
       icon: noti,
       heading: "NOTIFICATION TRADE",
       subHeading:
         "Buy or sell automatically whenever it pumps ordumps with our trading bot",
     },
     {
+      id: 3,
       icon: track,
       heading: "TRACK NEW GEMS",
       subHeading:
         "Find and trade new tokens before any other exchange knowabout it",
     },
     {
+      id: 4,
       icon: emotion,
       heading: "NO EMOTION SYSTEM",
       subHeading:
         "Get your tokens locked in yourwallet and avoid panic attackactions",
     },
     {
+      id: 5,
       icon: payment,
       heading: "PAYMENT GATEWAY",
       subHeading:
         "Our new payment gateway system will get attractionbecause of our early adoptersbeneficial programm",
     },
     {
+      id: 6,
       icon: govern,
       heading: "CRYPTOR GOVERNANCE",
       subHeading:
@@ -47,12 +58,14 @@ const WhatWeDo: React.FC = () => {
   ];
   const featureCard = features.map((data) => {
     return (
-      <div className="my-5">
+      <div className="my-5" key={data.id}>
         <div className="w-24 mx-auto">
-          <img className="w-full block" src={data.icon} />
+          <img className="w-full block" src={data.icon} alt="feature" />
         </div>
         <h2 className="text-center text-white text-lg"> {data.heading}</h2>
-        <p className="text-center text-gray-300 text-sm max-w-xs">{data.subHeading}</p>
+        <p className="text-center text-gray-300 text-sm max-w-xs">
+          {data.subHeading}
+        </p>
       </div>
     );
   });
@@ -64,7 +77,9 @@ const WhatWeDo: React.FC = () => {
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud
       </p>
-      <div className="grid grid-cols-3 gap-4 ms:grid-cols-2 mx-10">{featureCard}</div>
+      <div className="grid grid-cols-3 gap-4 ms:grid-cols-2 mx-10">
+        {featureCard}
+      </div>
     </div>
   );
 };
