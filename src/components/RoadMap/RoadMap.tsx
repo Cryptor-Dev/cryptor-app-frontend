264a50f17689159ecc7805ec4b1c54723bec47d5
faheelsattar <faheelsattar@yahoo.com>
2021-10-16 00:45:16 +0500

    roadmap svg rendered dynamically

--- src/components/RoadMap/RoadMap.tsx
+++ src/components/RoadMap/RoadMap.tsx
@@ -1,29 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import cryptorBg from "../../assets/cryptor-bg.svg";
 import Carousel from "../common/Carousel";
+import { ImgProvider } from "./imgProvider";
 const RoadMap: React.FC = () => {
+  const [activeSlide, setActiveSlide] = useState<number>(0);
+  type SLIDES_DATA = Array<{ id: number; heading: String; subHeading: String }>;
+  const slides: SLIDES_DATA = [
+    {
+      id: 0,
+      heading: "01. March 2021",
+      subHeading:
+        "Lorem ipsum dolor sit amet, consectetur wfwf adipiscing elit, sed do eiusmod tempor incididunt ut",
+    },
+    {
+      id: 1,
+
+      heading: "01. March 2021",
+      subHeading:
+        "Lorem ipsum dolor sit amet, consectetur wfwf adipiscing elit, sed do eiusmod tempor incididunt ut",
+    },
+    {
+      id: 2,
+
+      heading: "01. March 2021",
+      subHeading:
+        "Lorem ipsum dolor sit amet, consectetur wfwf adipiscing elit, sed do eiusmod tempor incididunt ut",
+    },
+    {
+      id: 3,
+      heading: "01. March 2021",
+      subHeading:
+        "Lorem ipsum dolor sit amet, consectetur wfwf adipiscing elit, sed do eiusmod tempor incididunt ut",
+    },
+    {
+      id: 4,
+      heading: "01. March 2021",
+      subHeading:
+        "Lorem ipsum dolor sit amet, consectetur wfwf adipiscing elit, sed do eiusmod tempor incididunt ut",
+    },
+  ];
+
+  const handleSlideNumber = (id: number): void => {
+    setActiveSlide(id);
+  };
   return (
     <div
-      className="w-full text-white text-center pt-20 sm:mt-28 space-y-5"
+      className="w-full text-white text-center pt-20 sm:mt-28 space-y-5 my-10"
       id="roadmap"
     >
-      <h1 className="text-2xl">Roadmap</h1>
       <p className="text-sm font-light"></p>
       <div
         style={{
           backgroundImage: `url(${cryptorBg})`,
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
+          backgroundSize: "contain",
         }}
       >
-        {/* <img
-          src={cryptorBg}
-          className=""
-          alt=""
-          style={{ width: "70%", display: "block", margin: "0 auto" }}
-        /> */}
-        <Carousel />
+        <h1 className="text-2xl">Roadmap</h1>
+        <div className="w-full p-6">
+          <ImgProvider id={activeSlide} />
+        </div>
+        <Carousel
+          handleSlideNumber={handleSlideNumber}
+          activeSlide={activeSlide}
+          slides={slides}
+        />
       </div>
     </div>
   );
--- src/components/common/Carousel.tsx
+++ src/components/common/Carousel.tsx
@@ -1,53 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import Slider from "react-slick";
 import "./Carousel.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const Carousel = () => {
-  const [activeSlide, setActiveSlide] = useState<number>(0);
-  type SLIDES_DATA = Array<{ id: number; heading: String; subHeading: String }>;
-  const slides: SLIDES_DATA = [
-    {
-      id: 0,
-      heading: "01. March 2021",
-      subHeading:
-        "Lorem ipsum dolor sit amet, consectetur wfwf adipiscing elit, sed do eiusmod tempor incididunt ut",
-    },
-    {
-      id: 1,
+interface IProps {
+  handleSlideNumber: (id: number) => void;
+  activeSlide: number;
+  slides: Array<{ id: number; heading: String; subHeading: String }>;
+}
 
-      heading: "01. March 2021",
-      subHeading:
-        "Lorem ipsum dolor sit amet, consectetur wfwf adipiscing elit, sed do eiusmod tempor incididunt ut",
-    },
-    {
-      id: 2,
-
-      heading: "01. March 2021",
-      subHeading:
-        "Lorem ipsum dolor sit amet, consectetur wfwf adipiscing elit, sed do eiusmod tempor incididunt ut",
-    },
-    {
-      id: 3,
-      heading: "01. March 2021",
-      subHeading:
-        "Lorem ipsum dolor sit amet, consectetur wfwf adipiscing elit, sed do eiusmod tempor incididunt ut",
-    },
-    {
-      id: 4,
-      heading: "01. March 2021",
-      subHeading:
-        "Lorem ipsum dolor sit amet, consectetur wfwf adipiscing elit, sed do eiusmod tempor incididunt ut",
-    },
-    {
-      id: 5,
-      heading: "01. March 2021",
-      subHeading:
-        "Lorem ipsum dolor sit amet, consectetur wfwf adipiscing elit, sed do eiusmod tempor incididunt ut",
-    },
-  ];
-
+const Carousel = (props: IProps) => {
   const settings = {
     className: "center",
     centerMode: true,
@@ -57,31 +20,29 @@ const Carousel = () => {
     speed: 2000,
     autoplay: true,
     cssEase: "linear",
-    beforeChange: (next: number) => setActiveSlide(-1),
-    afterChange: (current: number) => setActiveSlide(current),
+    beforeChange: (next: number) => props.handleSlideNumber(-1),
+    afterChange: (current: number) => props.handleSlideNumber(current),
   };
 
   return (
     <div>
-      <div>
         <p
           className={
-            activeSlide !== -1
+            props.activeSlide !== -1
               ? "text-green-500 text-center text-2xl font-bold"
               : "text-gray-300 text-center text-2xl font-bold"
           }
         >
           __
         </p>
-      </div>
       <Slider {...settings}>
-        {slides.map((data, i) => {
+        {props.slides.map((data, i) => {
           return (
             <div className="p-5">
               <h3
                 className="text-center text-2xl my-5"
                 style={
-                  activeSlide === data.id
+                  props.activeSlide === data.id
                     ? { color: "#ffffff" }
                     : { color: "rgb(177, 177, 177)" }
                 }
@@ -92,7 +53,7 @@ const Carousel = () => {
               <p
                 className="text-center text-sm"
                 style={
-                  activeSlide === data.id
+                  props.activeSlide === data.id
                     ? { color: "rgb(211, 211, 211)" }
                     : { color: "rgb(156, 156, 156)" }
                 }
